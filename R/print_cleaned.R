3becd113503ce3d9306bda2b7bf4d2f27f24364f
Bin <charles.bzhou@gmail.com>
2021-07-18 23:03:40 +0100

    Update data cleaning codes

--- R/print_cleaned.R
+++
@@ -1,9 +0,0 @@
-print_cleaned <- function(var) {
-    print(paste("Percentage Cleaned (No. of cleaned/No. of non-NAs):",var,"(%)"))
-    if (length(clnList)==0) {
-        print("No records cleaned")
-    } else {
-        cln.table <- table(data[clnList,]$id_study)/table(data[!is.na(data[var]),]$id_study)*100
-        print(sort(round(cln.table[which(cln.table!=Inf&cln.table>0)],2), decreasing=TRUE))
-    }
-}
