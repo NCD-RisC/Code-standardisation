94c582a549948c1ebda3fa8a689cda8dc48f9c8b
Bin <charles.bzhou@gmail.com>
2021-10-03 00:07:57 +0100

    update make_age_group.R

--- R/make_age_groups.R
+++ R/make_age_groups.R
@@ -6,7 +6,7 @@
 #' @return The 10-year age group and corresponding mean age of \code{age} accounting for \code{age_design_min} and \code{age_disgn_max}.
 #' @examples
 #' make_age_groups(29, 18, 200)
-#' make_age_groups(c(29,32,50,80,12,18), c(18,18,55,55,10,10), c(200,35,80,80,200,200))
+#' make_age_groups(c(29,32,50,80,12,18,80), c(18,18,55,55,10,10,18), c(200,35,80,80,200,200,200))
 #' 
 make_age_groups <- function(age, age_design_min, age_design_max, anthro = FALSE) {
   # mean age for open age groups
@@ -26,30 +26,25 @@ make_age_groups <- function(age, age_design_min, age_design_max, anthro = FALSE)
   # for 18+; adolescent ages are dealt with later: they are NA if present
   age_group0 <- findInterval(age, c(18, seq(20, 80, by = 10), 200), right = FALSE)  # findInterval is faster than cut; returns numerics starting 0
   age_min <- c(0, 18, seq(20, 80, by = 10))[age_group0 + 1]
-  age_max <- c(18, seq(20, 80, by = 10), 200)[age_group0 + 1]
+  age_max <- c(18, seq(20, 80, by = 10), 201)[age_group0 + 1] -1
 
   # update the first and last age group according to age range
   age_min_list <- which(age_min < age_design_min)
   age_min[age_min_list] <- age_design_min[age_min_list]
   age_max_list <- which(age_max > age_design_max)
-  age_max[age_max_list] <- age_design_max[age_max_list] + 1
+  age_max[age_max_list] <- age_design_max[age_max_list]
   
   # generate age group and mean age
-  addon <- stringi::stri_c("-", age_max - 1)
-  age_max200_list <- which(age_max == 200)
-  addon[age_max200_list] <- "+"
-  age_group <- stringi::stri_c(age_min, addon)  # stri_c is faster than paste0
-  # age_group <- gsub("-199", "\\+", age_group, perl = TRUE) # age_max of 200 becomes 199
-  # age_group <- stringi::stri_replace_all_regex(age_group, "-200", "\\+")
+  age_group <- stringi::stri_c(age_min, "-", age_max)  # stri_c is faster than paste0
   
   # calculate mean age naively
-  age_mean <- (age_min + age_max) / 2
+  age_mean <- (age_min + age_max + 1) / 2
   
   # use pre-determined mean age for for open-ended age groups
-  age_mean_list1 <- age_max200_list
+  age_mean_list1 <- which(age_max == 200)
   age_mean[age_mean_list1] <- mean_age_open_age_group$mean_age[match(age_min[age_mean_list1], mean_age_open_age_group$min_age)]
   # single year age groups: use nominal age rather than xx.5 as mid-age
-  age_mean_list2 <- which(age_max - age_min == 1)
+  age_mean_list2 <- which(age_max - age_min == 0)
   age_mean[age_mean_list2] <- age_min[age_mean_list2]
   
   # deal with adolescents who should have single year age groups
@@ -57,9 +52,11 @@ make_age_groups <- function(age, age_design_min, age_design_max, anthro = FALSE)
   adolescent_list <- which(age < ifelse(anthro, 20, 18))
   if (length(adolescent_list) > 0) {
     f_age <- floor(age[adolescent_list])
+    age_min[adolescent_list] <- f_age
+    age_max[adolescent_list] <- f_age
     age_group[adolescent_list] <- stringi::stri_c(f_age, "-", f_age)
     age_mean[adolescent_list] <- floor(age[adolescent_list])
   }
   
-  return(data.frame(age_mean, age_group))
+  return(data.frame(age_mean, age_group, age_min, age_max))   # ideally age_min and age_max should go
 }
